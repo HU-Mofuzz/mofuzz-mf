d3395badb4f05de225f48bb7882f126e24337196
LukasJue <juenemann.lukas@gmail.com>
2022-03-12 13:16:10 +0100

    Preparing everything for automated execution

--- src/main/java/de/hub/mse/emf/multifile/SvgTest.java
+++ src/main/java/de/hub/mse/emf/multifile/SvgTest.java
@@ -8,7 +8,6 @@ import de.hub.mse.emf.multifile.impl.svg.SvgGenerator;
 import edu.berkeley.cs.jqf.fuzz.Fuzz;
 import edu.berkeley.cs.jqf.fuzz.JQF;
 import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
-import org.apache.batik.anim.dom.SVGDOMImplementation;
 import org.apache.batik.transcoder.SVGAbstractTranscoder;
 import org.apache.batik.transcoder.TranscoderException;
 import org.apache.batik.transcoder.TranscoderInput;
@@ -35,9 +34,8 @@ public class SvgTest {
     private static int iteration = 0;
 
     @Fuzz
-    public void svgSalamanderTest(@From(SvgGenerator.class) File inputFile) throws InterruptedException, MalformedURLException {
+    public void svgSalamanderTest(@From(SvgGenerator.class) File inputFile) throws MalformedURLException {
         System.out.println("STARTING ITERATION "+ ++iteration);
-        long start = System.currentTimeMillis();
         System.out.println(inputFile.getAbsolutePath());
 
         SVGUniverse universe = SVGCache.getSVGUniverse();
@@ -58,31 +56,22 @@ public class SvgTest {
         var diagram = universe.getDiagram(inputFile.toURI());
 
         Assert.assertNotNull(diagram);
-
-        long duration = System.currentTimeMillis() - start;
     }
 
     @Fuzz
-    public void testBatik(@From(SvgGenerator.class) File inputFile) throws IOException, InterruptedException {
+    public void testBatik(@From(SvgGenerator.class) File inputFile) throws IOException {
         System.out.println("STARTING ITERATION "+ ++iteration);
-        long start = System.currentTimeMillis();
-
-        String svgNS = SVGDOMImplementation.SVG_NAMESPACE_URI;
         String parser = XMLResourceDescriptor.getXMLParserClassName();
         SAXSVGDocumentFactory f = new SAXSVGDocumentFactory(parser);
 
         SVGDocument doc = f.createSVGDocument(inputFile.toURI().toString());
 
         Assert.assertNotNull(doc);
-
-        long duration = System.currentTimeMillis() - start;
-        Thread.sleep(Math.max(0, 1000 - duration));
     }
 
     @Fuzz
-    public void testBatikTranscoder(@From(SvgGenerator.class) File inputFile) throws IOException, InterruptedException, TranscoderException {
-        System.out.println("STARTING ITERATION "+ ++iteration + " WITH FILE > "+inputFile.getAbsolutePath());
-        long start = System.currentTimeMillis();
+    public void testBatikTranscoder(@From(SvgGenerator.class) File inputFile) throws TranscoderException {
+        System.out.println("STARTING ITERATION "+ ++iteration);
 
         PNGTranscoder transcoder = new PNGTranscoder();
         transcoder.addTranscodingHint(SVGAbstractTranscoder.KEY_HEIGHT, 1000f);
@@ -91,17 +80,6 @@ public class SvgTest {
 
         var outStream = new ByteArrayOutputStream(0);
         TranscoderOutput output = new TranscoderOutput(outStream);
-        try {
-            transcoder.transcode(input, output);
-            Assert.assertTrue(outStream.size() > 0);
-            System.out.println("ITERATION "+ iteration + " SUCCESSFUL!");
-        } catch (Exception e) {
-            System.out.println("ITERATION "+ iteration + " FAILED!");
-            e.printStackTrace();
-            throw e;
-        } finally {
-            long duration = System.currentTimeMillis() - start;
-            Thread.sleep(Math.max(0, 1000 - duration));
-        }
+        transcoder.transcode(input, output);
     }
 }
