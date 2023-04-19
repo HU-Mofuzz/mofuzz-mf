ac93721ba6eee40ab90c9cb78ff24b1cb3b546aa
Laokoon <lukas@alotof.tech>
2023-04-20 01:04:51 +0200

    Adding basic modules for distributed fuzzing implementation

---
+++ mofuzz-mf-distributed/mofuzz-document-server/src/test/java/de/hub/mse/server/MofuzzDocumentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hub.mse.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MofuzzDocumentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
