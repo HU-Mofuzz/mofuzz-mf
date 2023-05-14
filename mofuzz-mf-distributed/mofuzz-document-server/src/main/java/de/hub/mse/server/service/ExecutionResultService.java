1ee436e0392b27357bd7ff628af7b7c874ba00b4
Laokoon <lukas@alotof.tech>
2023-05-14 20:22:49 +0200

    Adding logging to result reporting

--- mofuzz-mf-distributed/mofuzz-document-server/src/main/java/de/hub/mse/server/service/ExecutionResultService.java
+++ mofuzz-mf-distributed/mofuzz-document-server/src/main/java/de/hub/mse/server/service/ExecutionResultService.java
@@ -6,10 +6,12 @@ import de.hub.mse.server.repository.ClientDescriptorRepository;
 import de.hub.mse.server.repository.ExecutionResultRepository;
 import de.hub.mse.server.repository.ExperimentRepository;
 import de.hub.mse.server.repository.FileDescriptorRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ExecutionResultService {
 
     private final ExecutionResultRepository resultRepository;
@@ -31,13 +33,16 @@ public class ExecutionResultService {
     public void reportResult(ExecutionResult result) throws NotFoundException {
         result.sanitize();
 
-        if(!experimentRepository.existsById(result.getExperiment())
-        || !clientRepository.existsById(result.getOriginClient())
-        || !fileRepository.existsById(result.getFileDescriptor())) {
+        var experiment = experimentRepository.findById(result.getExperiment()).orElseThrow(NotFoundException::new);
+        var client = clientRepository.findById(result.getOriginClient()).orElseThrow(NotFoundException::new);
+
+        if(!fileRepository.existsById(result.getFileDescriptor())) {
             throw new NotFoundException();
         }
 
         result.setId(null);
         resultRepository.save(result);
+        log.info("Client [{}] reported result for file [{}] in experiment [{}]", client.getName(),
+                result.getFileDescriptor(), experiment.getDescription());
     }
 }
